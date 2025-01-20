8baf3f504a253fe5c7658442ea89e8a242457d0f
Navaneeth Krishna <navaneethkrishnamudumba@gmail.com>
2025-01-20 10:04:22 +0530

    added security group resource

--- main.tf
+++ main.tf
@@ -2,6 +2,43 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "aws_security_group" "monitoring_sg" {
+  name        = "monitoring-sg"
+  description = "Allow access to Prometheus and Grafana"
+
+  ingress {
+    description = "Allow HTTP traffic for Prometheus"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP traffic for Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "my_instance" {
   ami           = "ami-023a307f3d27ea427"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
